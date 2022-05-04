3d90226fd8475e18cb6d07676915376fd69f6b57
ha-jok <1273447417@qq.com>
2022-05-05 03:10:23 +0800

    理解官方代码，添加注释

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 	"net/http"
 	"websockt-char/service"
 )
-
+//设定参数
 var addr = flag.String("addr", ":8080", "http service address")
 
 //聊天室中间件
 func ServeHome(w http.ResponseWriter, r *http.Request) {
-	//输出访问日志
-	log.Println(r.URL,"abc")
+	//打印访问日志
+	log.Println(r.URL)
 	//检验路径是否正确
 	if r.URL.Path != "/" {
 		http.Error(w, "Not found", http.StatusNotFound)
@@ -32,13 +32,17 @@ func ServeHome(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	//解析参数
 	flag.Parse()
 	hub := service.NewHub()
 	go hub.Run()
+	//聊天室接口，聊天室界面
 	http.HandleFunc("/", ServeHome)
+	//监听接口，监听用户发送的信息，并将信息写入响应体里面
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		service.ServeWs(hub, w, r)
 	})
+	//监听8080端口信息
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
--- service/client.go
+++ service/client.go
@@ -11,39 +11,46 @@ import (
 	"net/http"
 	"time"
 )
-
+//定义基础信息，
 const (
 	// Time allowed to write a message to the peer.
+	//允许写信息的时长
 	writeWait = 10 * time.Second
 
 	// Time allowed to read the next pong message from the peer.
+	//允许读取下一条pong信息的时长
 	pongWait = 60 * time.Second
 
 	// Send pings to peer with this period. Must be less than pongWait.
+	//在此期间发送ping信息
 	pingPeriod = (pongWait * 9) / 10
 
 	// Maximum message size allowed from peer.
+	//信息最大的大小
 	maxMessageSize = 512
 )
-
+//声明换行符和空格
 var (
 	newline = []byte{'\n'}
 	space   = []byte{' '}
 )
-
-var upgrader = websocket.Upgrader{
+//升级协议相关配置
+var Upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
 
 // Client is a middleman between the websocket connection and the hub.
+//定义客户端的结构
 type Client struct {
 	hub *Hub
 
 	// The websocket connection.
+	//TCP的连接
 	conn *websocket.Conn
 
 	// Buffered channel of outbound messages.
+	//定义一个通道
 	send chan []byte
 }
 
@@ -52,14 +59,18 @@ type Client struct {
 // The application runs readPump in a per-connection goroutine. The application
 // ensures that there is at most one reader on a connection by executing all
 // reads from this goroutine.
+//读取信息
 func (c *Client) readPump() {
+	//延时关闭连接。
 	defer func() {
 		c.hub.unregister <- c
 		c.conn.Close()
 	}()
+	//对连接信息通道设置限制，
 	c.conn.SetReadLimit(maxMessageSize)
 	c.conn.SetReadDeadline(time.Now().Add(pongWait))
 	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	//从连接通道里面读取信息
 	for {
 		_, message, err := c.conn.ReadMessage()
 		if err != nil {
@@ -68,7 +79,9 @@ func (c *Client) readPump() {
 			}
 			break
 		}
+		//将换行符替换为空格
 		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		//将读取的信息传入广播通道
 		c.hub.broadcast <- message
 	}
 }
@@ -78,22 +91,27 @@ func (c *Client) readPump() {
 // A goroutine running writePump is started for each connection. The
 // application ensures that there is at most one writer to a connection by
 // executing all writes from this goroutine.
+//发送信息
 func (c *Client) writePump() {
 	ticker := time.NewTicker(pingPeriod)
+	//延时关闭连接
 	defer func() {
 		ticker.Stop()
 		c.conn.Close()
 	}()
+	//建立一个永久循环对信息通道和时间进行读取检验
 	for {
 		select {
 		case message, ok := <-c.send:
+			//设置时间限制
 			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
 			if !ok {
 				// The hub closed the channel.
+				//通道写入状态码
 				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
 				return
 			}
-
+			//将信息写入请求响应
 			w, err := c.conn.NextWriter(websocket.TextMessage)
 			if err != nil {
 				return
@@ -101,6 +119,7 @@ func (c *Client) writePump() {
 			w.Write(message)
 
 			// Add queued chat messages to the current websocket message.
+			//将send里面的信息读取到websockt中
 			n := len(c.send)
 			for i := 0; i < n; i++ {
 				w.Write(newline)
@@ -120,17 +139,21 @@ func (c *Client) writePump() {
 }
 
 // serveWs handles websocket requests from the peer.
+//监听中间件
 func ServeWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
+	//升级协议
+	conn, err := Upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
+	//读取用户信息
 	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
 	client.hub.register <- client
 
 	// Allow collection of memory referenced by the caller by doing all work in
 	// new goroutines.
+	//开启读写服务
 	go client.writePump()
 	go client.readPump()
 }
